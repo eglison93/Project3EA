070d3128fe182c221b8cb50a987f61aaaa2d3025
eglison93 <44655469+eglison93@users.noreply.github.com>
2018-12-10 17:25:37 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function InchConverter(val) {
+    document.getElementById("outputinch").innerHTML=val*2.54;
+    }
+
+ function CmConverter(val) {
+    document.getElementById("outputcm").innerHTML=val/2.54;
+    }
