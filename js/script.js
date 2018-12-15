017fbb7dd17f3eea613953a81fa3fb369f4b05f7
eglison93 <44655469+eglison93@users.noreply.github.com>
2018-12-15 15:56:09 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function InchConverter(val) {
-    document.getElementById("outputinch").innerHTML=val*2.54;
-    }
-
- function CmConverter(val) {
-    document.getElementById("outputcm").innerHTML=val/2.54;
-    }
